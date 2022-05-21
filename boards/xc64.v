fe9fea39ad65231860a98a62cc4ebf92de06f0a7
Ilia Platone <info@iliaplatone.com>
2022-05-21 03:08:23 +0200

    Wrong setting in XC32

--- boards/xc64.v
+++ boards/xc64.v
@@ -9,19 +9,19 @@
 module top_module (
 	sysclk,
 	jp1,
-	jp2
+	jp2 
 	);
-	
+
 parameter PLL_FREQUENCY = 400000000;
 parameter CLK_FREQUENCY = 10000000;
 parameter SIN_FREQUENCY = 50;
-parameter MUX_LINES = 16;
-parameter NUM_LINES = 4;
-parameter DELAY_SIZE = 4;
+parameter MUX_LINES = 2;
+parameter NUM_LINES = 32;
+parameter DELAY_SIZE = 0;
 parameter LAG_CROSS = 1;
 parameter LAG_AUTO = 1;
-parameter RESOLUTION = 8;
-parameter HAS_LEDS = 1;
+parameter RESOLUTION = 24;
+parameter HAS_LEDS = 0;
 parameter HAS_CROSSCORRELATOR = 1;
 parameter HAS_PSU = 0;
 parameter HAS_CUMULATIVE_ONLY = 0;
@@ -34,7 +34,7 @@ parameter USE_SOFT_CLOCK = 1;
 input wire sysclk;
 inout wire[19:0] jp1;
 inout wire[19:0] jp2;
-
+ 
 wire TX;
 wire RX;
 wire refclk;
@@ -42,16 +42,14 @@ wire enable;
 wire extclk;
 wire intclk;
 wire smpclk;
-wire spiclk;
 wire strobe;
 
 assign extclk = jp1[16];
-assign jp1[17] = refclk;
-assign jp1[18] = intclk;
+assign jp1[17] = intclk;
+assign strobe = jp1[18];
 assign jp1[19] = smpclk;
 
-assign strobe = jp2[16];
-assign enable = jp2[17];
+assign jp2[16] = refclk;
 assign jp2[18] = TX;
 assign RX = jp2[19];
 
@@ -59,23 +57,9 @@ wire[NUM_LINES-1:0] line_in;
 wire[NUM_LINES*4-1:0] line_out;
 wire[MUX_LINES-1:0] mux_out;
 
-assign line_in[0] = jp1[14];
-assign line_in[1] = jp1[12];
-assign line_in[2] = jp1[10];
-assign line_in[3] = jp1[8];
-assign jp1[15] = line_out[0];
-assign jp1[13] = line_out[1];
-assign jp1[11] = line_out[2];
-assign jp1[9] = line_out[3];
-assign jp1[6] = line_out[8];
-assign jp1[4] = line_out[9];
-assign jp1[2] = line_out[10];
-assign jp1[0] = line_out[11];
-assign jp1[7] = line_out[12];
-assign jp1[5] = line_out[13];
-assign jp1[3] = line_out[14];
-assign jp1[1] = line_out[15];
-assign jp2[15:0] = mux_out;
+assign jp2[17] = mux_out[0];
+assign line_in[15:0] = jp1[15:0];
+assign line_in[31:16] = jp2[15:0];
 
 main #(
 .CLK_FREQUENCY(CLK_FREQUENCY),
