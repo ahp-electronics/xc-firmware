f833783250f7be1b417e28325d5d9c26fbfc53f0
Ilia Platone <info@iliaplatone.com>
2022-06-05 13:52:20 +0200

    Maximum order set to 4

--- boards/xc64.v
+++ boards/xc64.v
@@ -30,7 +30,7 @@ parameter WORD_WIDTH = 1;
 parameter USE_UART = 1;
 parameter BINARY = 0;
 parameter USE_SOFT_CLOCK = 1;
-parameter MAX_ORDER = NUM_LINES*MUX_LINES;
+parameter MAX_ORDER = 4;
 
 input wire sysclk;
 inout wire[19:0] jp1;
