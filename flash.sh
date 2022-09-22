3d0b1814501e4eb224b8c10ef7d4fe52b4bd8bb4
Ilia Platone <info@iliaplatone.com>
2022-09-22 14:44:15 +0200

    Update flash.sh

--- flash.sh
+++ flash.sh
@@ -36,7 +36,7 @@ environment() {
 svf() {
 	svf="${PWD}/output/${implementation}/${1}.svf"
 	tmpfile="${PWD}/output/${implementation}/${1}.xcf"
-	binsize=$(( $( wc -c build/xc8/fpga_firmware_xc8.bit | cut -d ' ' -f 1 ) ))
+	binsize=$(( $( wc -c build/${implementation}/${project}_${implementation}.bit | cut -d ' ' -f 1 ) ))
 	endaddr=$(printf '0x%08X' $(( ${binsize} / 8 + 1)) )
 	echo $project ${implementation} $1
 	sed -e "s:PART:${part}:g" "${PWD}/boards/flash_${1}.xcf" | \
